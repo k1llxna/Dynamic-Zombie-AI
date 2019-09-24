4882e8d2331c1d270e8a8115d61354d5aa8d6005
k1llxna <44785075+k1llxna@users.noreply.github.com>
2019-09-23 23:26:01 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enemy : MonoBehaviour
+{
+    public int pointValue = 1;
+    public float speed;
+    public float sightRadius = 10f;
+    public GameObject deathEffect;
+
+    private Transform target;
+    private Vector3 force;
+
+	NavMeshAgent nm;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		nm = GetComponent<NavMeshAgent>();
+        speed = Random.Range(4.0f, 8f);
+    }
+    
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= sightRadius)
+        {
+            nm.SetDestination(target.position);
+        } 
+    }
+
+    public void Die()
+    {
+        Instantiate(deathEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+    }
+}
